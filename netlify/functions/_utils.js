0f3a90b1219ef23b4d26865810df4c075addd693
Samarth Viswanth <samarthv080@gmail.com>
2026-09-21 23:46:56 +0530

    chore: PiP 5.0 root configuration, package scripts, and cleanup of legacy PiP 4.0 files

--- netlify/functions/_utils.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/_utils.js
-const fs = require('fs'); const path = require('path');
-function saveJSON(name, data){
-  const dir = path.join(__dirname, '..', '..', 'tmp');
-  fs.mkdirSync(dir, { recursive: true });
-  fs.writeFileSync(path.join(dir, `${name}.json`), JSON.stringify(data, null, 2));
-}
-module.exports = { saveJSON };
